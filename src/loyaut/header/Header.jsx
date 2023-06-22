a14c035fa4b19a375e07862104d48c7974b2e23c
adil <adilusubaliev@mail.ru>
2023-06-22 09:42:31 +0600

    finish

--- src/loyaut/header/Header.jsx
+++ src/loyaut/header/Header.jsx
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import css from "./Header.module.css";
-import logo from "../../assets/images/nursidas-low-resolution-logo-black-on-transparent-background.png";
+import logo from "../../assets/images/nursidas-low-resolution-logo-white-on-black-background.png";
 
 const Header = () => {
+  const [basketWindow, setBasketWindow] = useState(css.basketWrapperClose);
+
+  const deleteCard = (ind) => {
+    const basketItems = JSON.parse(localStorage.getItem("basket") || "[]");
+    basketItems.splice(ind, 1);
+    localStorage.setItem("basket", JSON.stringify(basketItems));
+    location.reload();
+  };
+  const closeWindow = (e)=> {
+    e.stopPropagation()
+    setBasketWindow(css.basketWrapperClose)
+  };
+
+  const basketItems = JSON.parse(localStorage.getItem("basket"));
   return (
     <div className="container">
       <header className={css.header}>
@@ -11,8 +25,49 @@ const Header = () => {
           <img src={logo} alt="" />
         </Link>
         <h1>always in style</h1>
-        <button>Shoping card</button>
+        <button onClick={() => setBasketWindow(css.basketWrapper)}>
+          Shoping card
+        </button>
       </header>
+      <nav>
+        <Link to="/catalog/Hoodies">Hoodies</Link>
+        <Link to="/catalog/Dresses">Dresses</Link>
+        <Link to="/catalog/Suits">Suits</Link>
+        <Link to="/catalog/Shoes">Shoes</Link>
+        <Link to="/catalog/T-Shirts">T-Shirts</Link>
+        <Link to="/catalog/Jeans">Jeans</Link>
+        <Link to="/catalog/Jackets">Jackets</Link>
+        <Link to="/catalog/Bags">Bags</Link>
+      </nav>
+
+      <div className={basketWindow} onClick={closeWindow}>
+        <div className={css.basket}>
+          <h1>Shopping Cart</h1>
+          <hr />
+          <div className={css.cardBasketWrapper}>
+            {basketItems?.map((el, ind) => {
+              if (el) {
+                return (
+                  <div key={ind} className={css.cardBasket}>
+                    <img src="" alt="" />
+                    <div className={css.cardDes}>
+                      <h2>{el.name}</h2>
+                      <p>{el.text}</p>
+                    </div>
+                    <div className={css.cardDes}>
+                      <h2 className="size">size: {el.size.join(", ")}</h2>
+                      <h2 className="color">color: {el.color.join(", ")}</h2>
+                      <button onClick={() => deleteCard(ind)}>Удалить</button>
+                    </div>
+                  </div>
+                );
+              }
+            })}
+          </div>
+          <hr />
+          <h1>hui</h1>
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/detail/Detail.jsx
+++ src/pages/detail/Detail.jsx
@@ -1,7 +1,41 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import css from "./Detail.module.css";
+import { useGetClothesDetailQuery } from "../../store/ClothesSlice";
+import img from "../../assets/images/nursidas-low-resolution-logo-white-on-transparent-background.png"
 
 const Detail = () => {
-  return <div>Detail</div>;
+  const params = useParams();
+  const id = params.id;
+  const { data } = useGetClothesDetailQuery(id);
+  console.log(data);
+  console.log(id);
+
+  const addToBasket = (el) => {
+    const basketItems = JSON.parse(localStorage.getItem("basket") || "[]");
+    basketItems.push(el);
+    localStorage.setItem("basket", JSON.stringify(basketItems));
+    console.log(JSON.parse(localStorage.getItem("basket")));
+  };
+
+  if (data) {
+    return (
+      <div className="container">
+        <div className={css.detailWrapper}>
+        <div className={css.img}>
+          <img src={img} alt="" />
+        </div>
+        <div className={css.DetailDes}>
+          <h1>{data.name}</h1>
+          <p>{data.text}</p>
+          <p className="size">size: {data.size.join(', ')}</p>
+          <p className="color">color: {data.color.join(', ')}</p>
+          <button onClick={() => addToBasket(data)}> В крозину</button>
+        </div>
+      </div>
+      </div>
+    );
+  }
 };
 
 export default Detail;
